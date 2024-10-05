a6f9d2e4ecc9f680aa4a18b82a10c8992d1b7a9c
Mehdi Mejdoubi <130922789+MehdiMejdoubi@users.noreply.github.com>
2024-10-05 17:13:56 +0100

    A.2 - Handling network error in (fetch with async / await)

--- logic/controller.js
+++ logic/controller.js
@@ -1,11 +1,20 @@
 import { Model } from "./model.js";
 const modelInstance = new Model(); 
 
+//A.2 - Handling network error in (fetch with async / await)
 const fetchRecipes = async function(recipeName){
+  try {
     const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${recipeName}`);
     const data = await response.json();
     modelInstance.setList(data.data.recipes);
     console.log(modelInstance.getList());
+  } catch (error) {
+    if (error instanceof NetworkError){
+      console.error(`Network error occured while fetching recipes : ${error.message}`);
+    } else {
+      console.error(`Error status : ${error.status}`);
+    }
+  }
 }
 
 //XMLHttpRequest
@@ -20,12 +29,20 @@ const fetchRecipes = async function(recipeName){
     response.send();
 };*/
 
+//A.2 - Handling network error in (fetch with async / await)
 const fetchSelectedRecipe = async function(id){
-    const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-    const data = await response.json();
-    modelInstance.setShownRecipe(data);
-    console.log(modelInstance.getShownRecipe());
-    return modelInstance.getShownRecipe();
+    try {
+        const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
+        const data = await response.json();
+        modelInstance.setShownRecipe(data);
+        console.log(modelInstance.getShownRecipe());
+    } catch (error) {
+        if (error instanceof NetworkError) {
+            console.error(`Network error occured while fetching recipes : ${error.message}`);
+        } else {
+            console.error(`Error status : ${error.status}`);
+        }
+    }
 }
 
 //XMLHttpRequest
