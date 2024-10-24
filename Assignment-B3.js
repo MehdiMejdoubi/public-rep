41bd5a95373acf1fa3fe84c8f30f98027edecb9c
Mehdi Mejdoubi <130922789+MehdiMejdoubi@users.noreply.github.com>
2024-10-24 21:19:08 +0100

    B3/ Last enhacement

--- Assignment-B3.js
+++ Assignment-B3.js
@@ -1,86 +1,81 @@
 //Create a Queue Class that resolves promises.
 //Using an instance of the Queue class, resolve an ordered array of generated promises.
 // Queue class
-class Queue {
-    constructor(){
-        this.queue = [];
-        this.results = [];
-        this.isProcessing = false;
+class CustomQueue {
+  constructor() {
+    this.queue = [];
+    this.results = [];
+    this.isProcessing = false;
+  }
+
+  enqueue(promise) {
+    this.queue.push(promise);
+    if (!this.isProcessing) {
+      this.executeResolving();
     }
-    
-    enqueue(promise) {
-        this.queue.push(promise); 
-        if (!this.isProcessing) {
-            this.executeResolving();
-        }
+  }
+
+  async executeResolving() {
+    if (this.queue.length === 0) {
+      this.isProcessing = false;
+      return;
+    } else {
+      this.isProcessing = true;
+      const currentPromise = this.queue.shift();
+
+      try {
+        const result = await currentPromise();
+        console.log(`Resolved with ${result}`);
+        this.results.push(result);
+      } catch (error) {
+        console.log(`Rejected with ${error}`);
+      } finally {
+        this.executeResolving();
+      }
     }
-    
-    async executeResolving () { 
-        this.isProcessing = true;
-        while(this.queue.length > 0){
-            const currentPromise = this.queue.shift();
-            currentPromise.then((result) => {
-                console.log(`Resolved with ${result}`);
-                console.log(`-----------------------`);
-                this.results.push(`[${result}]`);
-            }).catch((error) => {
-                console.log(`Rejected with ${error}`);
-                console.log(`-----------------------`);
-            })
-            
-            /*try {
-                const result = await currentPromise;
-                console.log(`Resolved with ${result}`);
-                console.log(`-----------------------`);
-                this.results.push(`[${result}]`);
-            } catch(error) {
-                console.log(`Rejected with ${error}`);
-                console.log(`-----------------------`);
-            }*/
-            
-        }
-        this.isProcessing = false;
-    }
+  }
 }
 
-const integerOccurences = []; 
+const integerOccurences = [];
 
 const getUniqueIntegerPromise = () => {
-    const randomInteger = Math.round(Math.random() * 10);
-    console.log(`Random integer -> ${randomInteger}`);
-    
+  const randomInteger = Math.round(Math.random() * 10);
+  console.log(`Random integer -> ${randomInteger}`);
+
+  return () => {
     return new Promise((resolve, reject) => {
-        const randomDelay = Math.round(Math.random() * 2000) + 1000;
-        
-        setTimeout(() => {
-            console.log(`Timing : ${randomDelay}ms for ${randomInteger}`);
-            if(!integerOccurences.includes(randomInteger)) {
-                resolve(randomInteger);
-                integerOccurences.push(randomInteger);
-            }else reject(randomInteger);
-            
-        }, randomDelay);
+      const randomDelay = Math.round(Math.random() * 2000) + 1000;
+
+      setTimeout(() => {
+        if (!integerOccurences.includes(randomInteger)) {
+          resolve(randomInteger);
+          integerOccurences.push(randomInteger);
+        } else {
+          reject(randomInteger);
+        }
+      }, randomDelay);
     });
+  };
 };
 
-const queueInstance = new Queue();
+const queueInstance = new CustomQueue();
 const maxSize = 10;
 
 const processQueue = () => {
-    const promises = [];
-    for(let i = 0 ; i < 10 ; i++){
-        promises.push(getUniqueIntegerPromise());
-    }
-    
-    promises.forEach(promise => queueInstance.enqueue(promise));
-    
+  const promises = [];
+  for (let i = 0; i < maxSize; i++) {
+    promises.push(getUniqueIntegerPromise());
+  }
+
+  promises.forEach((promise) => queueInstance.enqueue(promise));
 };
 
 processQueue();
 
 setTimeout(() => {
-    console.log(queueInstance.results);
+  console.log(queueInstance.results);
 }, 20000);
+
 
 
 
