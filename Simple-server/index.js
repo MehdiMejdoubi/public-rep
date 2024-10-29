cbf1d74101e5a8b17d85a5fa2fd03caa362b6bda
DESKTOP-PDQ8PIK\ULTRAPC <mehdimejdoubi134@gmail.com>
2024-10-29 21:26:40 +0100

    Simple-server : Added Routing with nodeJS

--- Simple-server/index.js
+++ Simple-server/index.js
@@ -1,8 +1,19 @@
 //Create simple server using node js
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    res.end('hello from the server');
+    const pathName  = req.url;
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('hello from the overview');
+    }else if(pathName === '/product'){
+        res.end('Hello from product page');
+    }else {
+        res.writeHead(404, {
+           'Content-type': 'text/html',
+        })
+        res.end('<h1>Error</h1>');
+    }
 })
 
 server.listen(8000, '127.0.0.1', () => {
